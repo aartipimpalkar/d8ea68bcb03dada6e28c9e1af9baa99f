96ebca8be1a7c7efe9518f3a32a0f10a0b30aaf5
aartipimpalkar <75663189+aartipimpalkar@users.noreply.github.com>
2021-08-05 11:40:09 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -22,9 +22,7 @@ class Security {
         this.access3.position(100,290);
         this.access3.style('background', 'white');  
 
-        this.button3 = createButton('Check');
-        this.button3.position(100,320);
-        this.button3.style('background', 'lightgrey'); 
+        
     }
 
     display(){
@@ -45,12 +43,7 @@ class Security {
             }
         });
 
-        this.button3.mousePressed(() => {
-            if(system.authenticate(accessCode3,this.access3.value())){
-                this.button3.hide();
-                this.access3.hide();
-                score++;
-            }
+        
         });
 
     }
